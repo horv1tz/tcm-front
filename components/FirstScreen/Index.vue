19d47b0e81d740efafadacaf402ce06facc59a12
Leonid Dyachenko <horvitz@dvorfs.ru>
2024-06-01 08:44:37 +0300

    Added skeletons and loaders

--- components/FirstScreen/Index.vue
+++ components/FirstScreen/Index.vue
@@ -6,137 +6,191 @@
         <p class="header-title">Как бы вы хотели провести время</p>
       </header>
       <div class="content">
-        <screen-card v-for="category in categories" :key="category.id" :card="category" />
+        <screen-card-skeleton v-if="!ct || !ct.length"/>
+        <screen-card v-for="category in ct" :key="id" :card="category" :children="bc.getChildrenOf(category.id)" />
       </div>
+
       <button class="button" @click="goToApp()">Поехали</button>
     </div>
   </div>
 </template>
 
-<script setup>
-  import LogoIcon from '~/components/icons/LogoIcon.vue'
-  import ScreenCard from '~/components/FirstScreen/ScreenCard.vue'
-  import appRepository from '~/services/repositories/app-repository'
-  import { toRefs } from 'vue'
-  import { useSettingsStore } from '~/stores/settings'
+<script>
+import { ref, computed } from 'vue'
+import LogoIcon from '~/components/icons/LogoIcon.vue'
+import ScreenCard from '~/components/FirstScreen/ScreenCard.vue'
+import ScreenCardSkeleton from '~/components/FirstScreen/ScreenCardSkeleton.vue'
+// import appRepository from '~/services/repositories/app-repository' // Удалено, так как больше не используется
+import { useSettingsStore } from '~/stores/settings'
 
-  const settingsStore = useSettingsStore()
-  const goToApp = () => {
-    settingsStore.setInitialProject(false)
-  }
+import { storeToRefs } from "pinia";
+import { toRefs } from "vue";
 
-  const fetchCategory = await appRepository.fetchCategory()
-  const fetchSubCategory = await appRepository.fetchSubCategory()
+import axios from 'axios';
+import { APIURL } from '~/services/misc/constants.ts'
+import { Breadcrumbs } from '~/services/misc/breadcrumbs.ts'
 
-  const filterSubcategory = id => {
-    const filteredCategory = fetchSubCategory.filter(({ categoryId }) => categoryId === id)
-    return filteredCategory.map(subcategory => ({
-      id: subcategory.id,
-      name: subcategory.name
-    }))
-  }
+export default {
+  setup(props) {
 
-//   let categories = toRefs(
-//     fetchCategory.map(category => ({
-//       id: category.id,
-//       name: category.nameCategory,
-//       subcategory: filterSubcategory(category.id),
-//       isSelect: false
-//     }))
-//   )
+    const settingsStore = useSettingsStore()
+    const { categories } = storeToRefs(settingsStore);
+    const bc = new Breadcrumbs(categories.value)
 
-//   settingsStore.setCategories(categories)
+    const ct = bc.top_row
+    const goToApp = () => {
+      settingsStore.setInitialProject(false)
+    }
+
+    // Default should be empty for the skeleton to display
+    // const fetchCategory = ref([])
+    // const fetchSubCategory = ref([])
+
+    // const filterSubcategory = id => {
+    //   const filteredCategory = fetchSubCategory.value.filter(({
+    //     categoryId
+    //   }) => categoryId === id)
+    //   return filteredCategory.map(subcategory => ({
+    //     id: subcategory.id,
+    //     name: subcategory.name
+    //   }))
+    // }
+
+    // const categories = computed( () => {
+    //   return fetchCategory.value.map(category => ({
+    //     id: category.id,
+    //     name: category.name,
+    //     subcategory: filterSubcategory(category.id),
+    //     isSelect: false
+    //   }))
+    // }
+    // )
+
+    // Initiate API call to fetch main categories
+    // axios({
+    //    method: 'get',
+    //    url: `${APIURL}/api/category`,
+    //    headers: {
+    //         accept: "application/json"
+    //       }
+    //  }).then(r => {
+    //     fetchCategory.value = r.data.map(category => ({
+    //       id: category.id,
+    //       name: category.name,
+    //       subcategory: filterSubcategory(category.id),
+    //       isSelect: false
+    //   }))
+      
+    //     settingsStore.setCategories(fetchCategory.value)
+    //  })
+
+    // // Initiate API call to fetch subcats
+    // axios({
+    //    method: 'get',
+    //    url: `${APIURL}/api/category/subCategory`,
+    //    headers: {
+    //         accept: "application/json"
+    //       }
+    //  }).then(r => {
+    //     fetchSubCategory.value = r.data
+    //  })
+
+    return {categories, goToApp, ct, bc}
+  },
+  components: { ScreenCard, ScreenCardSkeleton }
+}
+
 </script>
 
 <style lang="scss" scoped>
-  .first-screen {
-    background: url('~/assets/images/first-screen-bg.png') center center / cover no-repeat;
-    width: 100%;
-    height: 100vh;
-    transition: 0.6s 0.135s;
+.first-screen {
+  background: url('~/assets/images/first-screen-bg.png') center center / cover no-repeat;
+  width: 100%;
+  height: 100vh;
+  transition: 0.6s 0.135s;
 
-    &.hidden-screen {
-      opacity: 0;
-
-      .header {
-        .logo {
-          top: -1000px;
-        }
-        &-title {
-          left: -1000px;
-        }
-      }
-      .content {
-        left: -1000px;
-      }
-      .button {
-        transform: scale(0);
-      }
-    }
+  &.hidden-screen {
+    opacity: 0;
 
     .header {
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      margin-top: 40px;
-
       .logo {
-        position: relative;
-        top: 0;
-        transition: 0.8s;
+        top: -1000px;
       }
-
       &-title {
-        margin-top: 20px;
-        width: 60%;
-        text-align: center;
-        color: $white-color;
-
-        position: relative;
-        left: 0;
-        transition: 0.8s 0.05s;
+        left: -1000px;
       }
     }
     .content {
-      margin-top: 40px;
-      display: flex;
-      flex-direction: column;
-      gap: 12px;
-      max-height: 320px;
-      overflow-y: auto;
+      left: -1000px;
+    }
+    .button {
+      transform: scale(0);
+    }
+  }
+
+  .header {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 40px;
+
+    .logo {
+      position: relative;
+      top: 0;
+      transition: 0.8s;
+    }
+
+    &-title {
+      margin-top: 20px;
+      width: 60%;
+      text-align: center;
+      color: $white-color;
 
       position: relative;
       left: 0;
-      transition: 0.6s 0.07s;
-
-      &::-webkit-scrollbar {
-        width: 6px;
-
-        &-track {
-          background: transparent; /* цвет дорожки */
-        }
-        &-thumb {
-          background-color: rgb(0 0 0 / 20%);
-          border-radius: 3px;
-          border: 1px solid transparent;
-        }
-      }
-    }
-    .button {
-      margin-top: 40px;
-      width: 100px;
-      height: 100px;
-      border-radius: 50px;
-      background: linear-gradient(39deg, #fcc879 0%, #fe8f7e 100%);
-      color: $white-color;
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      margin-inline: auto;
-      cursor: pointer;
-      border: none;
-
-      transition: 0.6s 0.1s;
+      transition: 0.8s 0.05s;
     }
   }
+  .content {
+    margin-top: 40px;
+    display: flex;
+    flex-direction: column;
+    gap: 12px;
+    max-height: 320px;
+    overflow-y: auto;
+
+    position: relative;
+    left: 0;
+    transition: 0.6s 0.07s;
+
+    &::-webkit-scrollbar {
+      width: 6px;
+
+      &-track {
+        background: transparent; /* цвет дорожки */
+      }
+      &-thumb {
+        background-color: rgb(0 0 0 / 20%);
+        border-radius: 3px;
+        border: 1px solid transparent;
+      }
+    }
+  }
+  .button {
+    margin-top: 40px;
+    width: 100px;
+    height: 100px;
+    border-radius: 50px;
+    background: linear-gradient(39deg, #fcc879 0%, #fe8f7e 100%);
+    color: $white-color;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-inline: auto;
+    cursor: pointer;
+    border: none;
+
+    transition: 0.6s 0.1s;
+  }
+}
 </style>
