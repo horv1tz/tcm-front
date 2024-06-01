6d1ec35c047ee2ee0db0705428c2b7140a24219b
Leonid Dyachenko <horvitz@dvorfs.ru>
2024-06-01 08:37:40 +0300

    Daily tasks and category lists rewritten to use setup-based options API

--- components/CategoryList.vue
+++ components/CategoryList.vue
@@ -1,60 +1,149 @@
 <template>
-  <div class="category-list">
+  <div class="cl-skeleton ns pd2" v-if="!categories?.top_row || !categories?.top_row?.length">
+    <p>Составляем список предложений...</p>
+    <div class="sc-skeleton-wipe"></div>
+  </div>
+  <div class="category-list pd2" v-if="categories.top_row.length !== 0">
     <button
-      v-for="category in categories"
-      :key="category.value.id"
-      :class="['category-button', { 'category-active': activeId === category.value.id }]"
-      @click="emit('select-category', category.value.id)"
+      v-for="category in categories.top_row"
+      :key="category.id"
+      :class="['category-button', { 'category-active': activeId === category.id }]"
+      @click="setcat(category.id)"
     >
-      {{ category.value.name }}
+      {{ category.name }}
     </button>
   </div>
 </template>
 
-<script setup lang="ts">
-  import { useSettingsStore } from '~/stores/settings'
-  import { storeToRefs } from 'pinia'
-  import { toRefs } from 'vue'
+<script lang="ts">
+import { useSettingsStore } from "~/stores/settings";
+import { storeToRefs } from "pinia";
+import { toRefs } from "vue";
+import ArrowRight from '~/components/icons/ArrowRight.vue'
 
-  interface IProps {
-    activeId: number
+export default {
+  components:{ArrowRight},
+  props:['bc', 'active-id'],
+  setup(props, ctx) {
+    const activeId = ref(props['activeId']);
+    const categories = ref(props['bc'])
+
+    if (!categories.value) {
+      categories.value = {};
+      categories.value.top_row = []
+    }
+
+    const setcat = (id) => {
+      ctx.emit('select-category', id)
+      activeId.value = id
+    }
+
+    return {
+      categories,
+      activeId,
+      setcat
+    }
   }
+}
 
-  const settingsStore = useSettingsStore()
-  const { categories } = storeToRefs(settingsStore)
 
-  const props = defineProps<IProps>()
-  const { activeId } = toRefs(props)
 
-  const emit = defineEmits<{
-    'select-category': [categoryId: number]
-  }>()
 </script>
 
 <style lang="scss" scoped>
-  .category-list {
-    display: flex;
-    gap: 12px;
-    flex-wrap: wrap;
+@mixin keyframes($name) {
+  @keyframes #{$name} {
+    @content;
   }
-  .category-button {
-    border-radius: 4.5px;
-    background: rgba(188, 154, 255, 0.85);
-    box-shadow: 0 9px 18px 0 rgba(0, 0, 0, 0.05);
-    border: none;
-    padding: 12px 28px;
-    cursor: pointer;
+}
 
-    color: $white-color;
-    text-align: center;
-    font-size: 11.25px;
-    font-weight: 600;
-    line-height: 110%;
-    letter-spacing: -0.225px;
+@mixin animate($animation, $duration, $method, $times) {
+  animation: $animation $duration $method $times;
+}
 
-    &.category-active {
-      background: $white-color;
-      color: $black-color;
+.ns {
+  -webkit-touch-callout: none;
+  -webkit-user-select: none;
+  -khtml-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+
+.category-list {
+  display: flex;
+  gap: 12px;
+  flex-wrap: wrap;
+}
+.category-button {
+  border-radius: 4.5px;
+  background: rgba(188, 154, 255, 0.85);
+  box-shadow: 0 9px 18px 0 rgba(0, 0, 0, 0.05);
+  border: none;
+  padding: 12px 28px;
+  cursor: pointer;
+
+  color: $white-color;
+  text-align: center;
+  font-size: 11.25px;
+  font-weight: 600;
+  line-height: 110%;
+  letter-spacing: -0.225px;
+
+  &.category-active {
+    background: $white-color;
+    color: $black-color;
+  }
+}
+
+.pd2 {
+  margin-top: 1em;
+}
+
+.cl-skeleton {
+  border-radius: 4.5px;
+  background: rgba(93, 84, 110, 0.85);
+  box-shadow: 0 9px 18px 0 rgba(0, 0, 0, 0.05);
+  border: none;
+  padding: 12px 28px;
+  cursor: pointer;
+
+  margin-top: 1em;
+
+  color: $white-color;
+  text-align: center;
+  font-size: 16.25px;
+  font-weight: 600;
+  line-height: 110%;
+  letter-spacing: -0.225px;
+  overflow: hidden;
+  position: relative;
+}
+
+.sc-skeleton-wipe {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-size: 200% 100% !important;
+  background-repeat: no-repeat !important;
+  background-position: 100% 0%;
+  background: linear-gradient(
+    248deg,
+    rgba(0, 0, 0, 0) 43%,
+    rgba(255, 255, 255, 0.2) 46%,
+    rgba(0, 0, 0, 0) 49%
+  );
+  cursor: wait;
+  @include keyframes(wipe) {
+    0% {
+      background-position: 120% 0%;
+    }
+    100% {
+      background-position: 0% 0%;
     }
   }
+  @include animate(wipe, 1s, cubic-bezier(0.79, 0.14, 0.15, 0.86), infinite);
+}
 </style>
