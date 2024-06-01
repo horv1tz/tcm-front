c5626351f413425e3fb4423fa8cb06f3f103dd8b
Leonid Dyachenko <horvitz@dvorfs.ru>
2024-06-01 08:47:34 +0300

    WIP, store was set to static to prepare for the new API backend

--- stores/settings.ts
+++ stores/settings.ts
@@ -4,15 +4,25 @@ import { ISettingState, ICategoryList } from '~/services/interfaces/index'
 export const useSettingsStore = defineStore('settings', {
   state: (): ISettingState => ({
     initialProject: true,
-    categories: []
+    categories: [
+      {"id":1,"name":"Развлечения"},
+      {"id":2,"name":"Еда"},
+      {"id":3,"name":"Отдых"},
+      {"id":4,"name":"Кино", "parentId":1},
+      {"id":5,"name":"Парки развлечений", "parentId":1},
+      {"id":6,"name":"Музеи", "parentId":1},
+      {"id":7,"name":"Арт-музеи", "parentId":6},
+      {"id":8,"name":"Исторические музеи", "parentId":6},
+    ]
   }),
   getters: {},
   actions: {
     setInitialProject(initialProject: boolean): void {
       this.initialProject = initialProject
     },
-    setCategories(categories: ICategoryList[]): void {
-      this.categories = categories
+    setCategories(categories: any[]): void {
+      // console.debug('Storing categories', categories)
+      // this.categories = categories
     }
   }
 })
